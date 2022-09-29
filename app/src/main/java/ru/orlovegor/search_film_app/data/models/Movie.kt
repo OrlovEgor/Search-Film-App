1123e557c277f7fe70ac77352735a5422b2e0a53
Orlov Egor <dbn.orlov@gmail.com>
2022-09-30 00:50:08 +0300

    add data class Movie

--- app/src/main/java/ru/orlovegor/search_film_app/data/models/Movie.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/models/Movie.kt
@@ -1,4 +1,14 @@
 package ru.orlovegor.search_film_app.data.models
 
-class Movie {
-}
+import com.squareup.moshi.Json
+import ru.orlovegor.search_film_app.data.models.remote_models.PosterMovie
+import ru.orlovegor.search_film_app.data.models.remote_models.RatingMovie
+
+data class Movie(
+    val id: Long,
+    val title: String,
+    val releaseDate: String,
+    val description: String,
+    val rating : Double,
+    val posterUrl: String,
+)
