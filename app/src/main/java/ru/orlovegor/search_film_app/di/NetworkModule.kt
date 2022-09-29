732790752e8f1bda8182e14419a9a48abb1b7621
Orlov Egor <dbn.orlov@gmail.com>
2022-09-30 00:51:38 +0300

    test injections TODO: move to the correct module

--- app/src/main/java/ru/orlovegor/search_film_app/di/NetworkModule.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/di/NetworkModule.kt
@@ -1,14 +1,21 @@
 package ru.orlovegor.search_film_app.di
 
+import android.app.Application
+import android.app.ApplicationErrorReport
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import ru.orlovegor.search_film_app.app.App
 import ru.orlovegor.search_film_app.data.models.MovieApi
+import ru.orlovegor.search_film_app.data.repositories.SearchMovieRepositoryImpl
+import ru.orlovegor.search_film_app.domain.GetMovieByTittleUsesCase
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -18,6 +25,10 @@ object NetworkModule {
     private const val queryName = "token"
     private const val API_KEY = "N45CTXD-M7A4S5S-NZYANQ2-5JDQDRF"
     private const val Base_URL = "https://api.kinopoisk.dev"
+
+    @Provides
+    fun provideUsesCase(@ApplicationContext context: Context, searchMovieRepositoryImpl: SearchMovieRepositoryImpl): GetMovieByTittleUsesCase =
+        GetMovieByTittleUsesCase(context,searchMovieRepositoryImpl)
 
 
     @Provides
