f223b065855ac75168c6b5856db7aaa91b59f90d
Orlov Egor <dbn.orlov@gmail.com>
2022-10-04 00:42:55 +0300

    refactor create Paging source from default factory

--- app/src/main/java/ru/orlovegor/search_film_app/di/RepositoryModule.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/di/RepositoryModule.kt
@@ -3,11 +3,11 @@ package ru.orlovegor.search_film_app.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 import ru.orlovegor.search_film_app.data.repositories.SearchMovieRepository
 import ru.orlovegor.search_film_app.data.repositories.SearchMovieRepositoryImpl
 
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 abstract class RepositoryModule {
 
