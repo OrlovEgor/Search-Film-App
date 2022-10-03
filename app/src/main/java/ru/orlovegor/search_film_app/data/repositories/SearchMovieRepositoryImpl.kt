e1d147e781e4d7b5f5bba83c1b047c390247a1fd
Orlov Egor <dbn.orlov@gmail.com>
2022-10-04 00:42:19 +0300

    refactor create Paging source from default factory

--- app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepositoryImpl.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepositoryImpl.kt
@@ -2,24 +2,21 @@ package ru.orlovegor.search_film_app.data.repositories
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import kotlinx.coroutines.flow.Flow
 import ru.orlovegor.search_film_app.data.models.MovieApi
-import ru.orlovegor.search_film_app.data.models.MoviesPageSource
+import ru.orlovegor.search_film_app.data.paging.MoviesPageSource
 import ru.orlovegor.search_film_app.data.models.remote_models.MovieDto
-import ru.orlovegor.search_film_app.data.models.remote_models.RemoteSearchResult
 import javax.inject.Inject
 
 class SearchMovieRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
 ) : SearchMovieRepository {
-    override  fun getMovieByTittlePaging(tittle: String): Pager<Int,MovieDto> {
-        return Pager<Int, MovieDto>(
-            PagingConfig(pageSize = 5 )
-        ) {
-            MoviesPageSource(movieApi, tittle)
-        }
+    override fun getMovieByTittlePaging(tittle: String): Pager<Int, MovieDto> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 5
+            ),
+            pagingSourceFactory = { MoviesPageSource(movieApi, tittle) }
+        )
     }
-
 
 }
