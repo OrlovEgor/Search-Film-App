3e6f6dd7d4a7d540acd9c091d53921e832b6cdc0
Orlov Egor <dbn.orlov@gmail.com>
2022-10-04 00:47:59 +0300

    change VM logic after add uses case

--- app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MovieSearchViewModel.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MovieSearchViewModel.kt
@@ -1,36 +1,27 @@
 package ru.orlovegor.search_film_app.presentation.movie_search
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
 import androidx.paging.PagingData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import ru.orlovegor.search_film_app.data.models.remote_models.MovieDto
-import ru.orlovegor.search_film_app.data.repositories.SearchMovieRepositoryImpl
+import ru.orlovegor.search_film_app.domain.GetMovieByTittleUsesCase
 import javax.inject.Inject
-
 
 @HiltViewModel
 class MovieSearchViewModel @Inject constructor(
-    private val searchMovieRepositoryImpl: SearchMovieRepositoryImpl
+    private val usesCase: GetMovieByTittleUsesCase
 ) : ViewModel() {
 
     private val _query = MutableStateFlow("")
     private val query: StateFlow<String> = _query.asStateFlow()
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     val movies: StateFlow<PagingData<MovieDto>> = query
-        .map(::getMovies)
-        .flatMapLatest { pager -> pager.flow }
-        .flowOn(Dispatchers.IO)
+        .mapLatest { usesCase.invoke(it) }
         .stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
-
-
-    private fun getMovies(tittle: String): Pager<Int, MovieDto> {
-        return searchMovieRepositoryImpl.getMovieByTittlePaging(tittle)
-    }
 
     fun setQuery(query: String) {
         _query.tryEmit(query)
