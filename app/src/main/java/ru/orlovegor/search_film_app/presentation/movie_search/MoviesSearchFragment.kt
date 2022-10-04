b14a788188999e893a5b56782be1f69f93a567f4
Orlov Egor <dbn.orlov@gmail.com>
2022-10-05 01:28:43 +0300

    added decorator, and error snack

--- app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MoviesSearchFragment.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MoviesSearchFragment.kt
@@ -1,14 +1,20 @@
 package ru.orlovegor.search_film_app.presentation.movie_search
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import ru.orlovegor.search_film_app.R
@@ -36,22 +42,44 @@ class MoviesSearchFragment : Fragment(R.layout.fragment_search_movies) {
         }
         initSearchView()
         movieAdapter.addLoadStateListener { state ->
+            val refreshError = state.refresh
             binding.movieProgressBarHorizontal.isVisible =
                 state.append == LoadState.Loading || state.refresh == LoadState.Loading
+
+            if (state.refresh is LoadState.Error || state.append is LoadState.Error){
+                Log.d("ERROR!!!!!!!!!!!!!!!!", "${refreshError.toString()}")
+                showSnack()
+
+            }
+
         }
     }
 
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun initList() {
+        val dividerDecorator = DividerItemDecoration(requireContext(), RecyclerView.VERTICAL)
+        dividerDecorator.setDrawable(
+            resources.getDrawable(
+                R.drawable.divider_drawable,
+                requireContext().theme
+            )
+        )
         with(binding.movieRecyclerView) {
             adapter = movieAdapter
             layoutManager = LinearLayoutManager(requireContext())
-            TODO("Добавить разделители + анимацию прокрутки." +
-                    " так же добавить плэйсхолдеры на загрузку изображения и ошибку загрузки избражения," +
-                    "ДОбавить интерцепторы в диай : на добаление хэдэра и на проверку подключения к сети. " +
-                    "обработать эти ошибки в пэйджинге и вывести пользователю снэкбар" +
-                    ". добавить текст вью при пустом списке с тектстом пока ничего не найдено.")
+            addItemDecoration(dividerDecorator)
 
+            /*TODO( " " +
+                    " +
+                    "обработать эти ошибки в пэйджинге и вывести пользователю снэкбар" +
+                    ". добавить текст вью при пустом списке с тектстом пока ничего не найдено.")*/
         }
+    }
+
+    private fun showSnack(){
+        Snackbar.make(binding.root,getString(R.string.no_connection), Snackbar.LENGTH_LONG)
+            .show()
     }
 
     private fun initSearchView() {
@@ -61,11 +89,11 @@ class MoviesSearchFragment : Fragment(R.layout.fragment_search_movies) {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 val tittle = binding.movieSearchView.query.toString()
                 viewModel.setQuery(tittle)
-                return true
+                return false
             }
 
             override fun onQueryTextChange(p0: String?): Boolean {
-                return true
+                return false
             }
         })
     }
