65a4f273a3dfaf4bc3e1a13e8140b5a08b82c3d1
Orlov Egor <dbn.orlov@gmail.com>
2022-10-05 01:31:23 +0300

    Change cold flow to hot flow

--- app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MovieSearchViewModel.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MovieSearchViewModel.kt
@@ -7,6 +7,7 @@ import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import ru.orlovegor.search_film_app.data.models.Movie
 import ru.orlovegor.search_film_app.domain.GetMovieByTittleUsesCase
 import javax.inject.Inject
@@ -16,8 +17,8 @@ class MovieSearchViewModel @Inject constructor(
     private val usesCase: GetMovieByTittleUsesCase
 ) : ViewModel() {
 
-    private val _query = MutableStateFlow("")
-    private val query: StateFlow<String> = _query.asStateFlow()
+    private val _query = MutableSharedFlow<String>()
+    private val query = _query.asSharedFlow()
 
     @OptIn(ExperimentalCoroutinesApi::class)
     val movies: StateFlow<PagingData<Movie>> = query
@@ -27,7 +28,9 @@ class MovieSearchViewModel @Inject constructor(
 
 
     fun setQuery(query: String) {
-        _query.tryEmit(query)
+        viewModelScope.launch {
+            _query.emit(query)
+        }
     }
 
 }
