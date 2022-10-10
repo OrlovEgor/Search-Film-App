4426fe757f81b427eb4cd3860743b60c603bff42
Orlov Egor <dbn.orlov@gmail.com>
2022-10-11 01:43:46 +0300

    add fun getMovieFullBytittle

--- app/src/main/java/ru/orlovegor/search_film_app/data/remote/MovieApi.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/remote/MovieApi.kt
@@ -4,6 +4,7 @@ import androidx.annotation.IntRange
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import ru.orlovegor.search_film_app.data.remote.models.MovieDto
 import ru.orlovegor.search_film_app.data.remote.models.RemoteSearchResult
 
 interface MovieApi {
@@ -15,6 +16,11 @@ interface MovieApi {
         @Query("limit") @IntRange(from = 1, to = MAX_PAGE_SIZE.toLong()) pageSize: Int = DEFAULT_PAGE_SIZE,
         @Query("isStrict") isStrict: Boolean = false
     ): Response<RemoteSearchResult>
+
+    @GET("/movie?field=id")
+    suspend fun getFullDescriptionMovie(
+       @Query("search") id:Long
+    ): MovieDto
 
     companion object {
         const val DEFAULT_PAGE_SIZE = 10
