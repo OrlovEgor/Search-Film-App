a13e7c6a880ad97bc1825f2e0402b5c0e69185ed
Orlov Egor <dbn.orlov@gmail.com>
2022-10-11 01:40:14 +0300

    add properties

--- app/src/main/java/ru/orlovegor/search_film_app/data/models/Movie.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/models/Movie.kt
@@ -1,14 +1,16 @@
 package ru.orlovegor.search_film_app.data.models
 
-import com.squareup.moshi.Json
-import ru.orlovegor.search_film_app.data.models.remote_models.PosterMovie
-import ru.orlovegor.search_film_app.data.models.remote_models.RatingMovie
+import ru.orlovegor.search_film_app.data.remote.models.SimilarMovieDto
 
 data class Movie(
     val id: Long,
     val title: String,
+    val posterUrl: String,
     val releaseDate: String,
     val description: String,
     val rating : Double,
-    val posterUrl: String,
+    val ageRestriction: String,
+    val similarMovieDto: List<SimilarMovieDto>
+
 )
+
