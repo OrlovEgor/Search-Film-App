e6aca581a4e573dfedb497bd76ad99ef607542e7
Orlov Egor <dbn.orlov@gmail.com>
2022-10-11 01:37:14 +0300

    added properties ageRestriction, similarMovie,  and support classes

--- app/src/main/java/ru/orlovegor/search_film_app/data/remote/models/MovieDto.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/remote/models/MovieDto.kt
@@ -1,4 +1,4 @@
-package ru.orlovegor.search_film_app.data.models.remote_models
+package ru.orlovegor.search_film_app.data.remote.models
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
@@ -12,14 +12,18 @@ data class MovieDto(
     @Json(name = "year")
     val releaseDate: String?,
     @Json(name = "description")
-    val description: String? ,
+    val description: String?,
     @Json(name = "shortDescription")
     val shortDescription: String?,
     @Json(name = "rating")
-    val rating : RatingMovie,
+    val rating: RatingMovie,
     @Json(name = "poster")
     val posterUrl: PosterMovie?,
-    )
+    @Json(name = "ratingMpaa")
+    val ageRestriction: AgeRestriction?,
+    @Json(name = "similarMovies")
+    val similarMovieDto: List<SimilarMovieDto?>?
+)
 
 @JsonClass(generateAdapter = true)
 data class PosterMovie(
@@ -32,10 +36,39 @@ data class PosterMovie(
 @JsonClass(generateAdapter = true)
 data class RatingMovie(
     @Json(name = "kp")
-    val kp : Double = 0.0,
+    val kp: Double = 0.0,
     @Json(name = "imdb")
     val imdb: Double = 0.0
 )
+
+@JsonClass(generateAdapter = true)
+data class SimilarMovieDto(
+    @Json(name = "id")
+    val id: Long?,
+    @Json(name = "name")
+    val tittle: String?,
+    @Json(name = "poster")
+    val imageUrl: PosterMovie?
+
+)
+
+enum class AgeRestriction(val age: String) {
+    @Json(name = "g")
+    GENERAL("0+"),
+
+    @Json(name = "pg")
+    PG("6+"),
+
+    @Json(name = "pg13")
+    PG_13("13+"),
+
+    @Json(name = "r")
+    R("16+"),
+
+    @Json(name = "nc17")
+    NC_17("18+")
+}
+
 
 
 
