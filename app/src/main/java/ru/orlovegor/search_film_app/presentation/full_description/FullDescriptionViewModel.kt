e5c4272b466563755cec15ed93c5f2b01b7cd042
Orlov Egor <dbn.orlov@gmail.com>
2022-10-10 21:31:04 +0300

    Added empty view model

---
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionViewModel.kt
@@ -0,0 +1,12 @@
+package ru.orlovegor.search_film_app.presentation.full_description
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FullDescriptionViewModel @Inject constructor (
+
+): ViewModel() {
+
+}
