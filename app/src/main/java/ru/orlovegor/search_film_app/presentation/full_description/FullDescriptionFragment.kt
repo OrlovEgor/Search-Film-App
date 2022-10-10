75e1cba3d19b485544de8f0cfcdce38af7c23b4b
Orlov Egor <dbn.orlov@gmail.com>
2022-10-10 21:31:30 +0300

    Added Hilt annotation

--- app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionFragment.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 import ru.orlovegor.search_film_app.R
 import ru.orlovegor.search_film_app.databinding.FragmentFullDescriptionBinding
 
+@AndroidEntryPoint
 class FullDescriptionFragment : Fragment(R.layout.fragment_full_description) {
 
     private val binding: FragmentFullDescriptionBinding by viewBinding()
