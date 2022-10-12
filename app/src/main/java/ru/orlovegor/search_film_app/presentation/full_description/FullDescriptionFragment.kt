3ea70fdaf1b1c6cbf3fca7ec3b3036c53f23fd8c
Orlov Egor <dbn.orlov@gmail.com>
2022-10-13 02:29:30 +0300

    error handle and change adapter references

--- app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionFragment.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionFragment.kt
@@ -3,11 +3,14 @@ package ru.orlovegor.search_film_app.presentation.full_description
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.StringRes
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import ru.orlovegor.search_film_app.R
@@ -19,45 +22,46 @@ class FullDescriptionFragment : Fragment(R.layout.fragment_full_description) {
     private val binding: FragmentFullDescriptionBinding by viewBinding()
     private val viewModel: FullDescriptionViewModel by viewModels()
     private val fullDescriptionMovieAdapter by lazy(LazyThreadSafetyMode.NONE) {
-        FullDescriptionMovieAdapter { movieId -> viewModel.loadSimilarMovieFullDescription(movieId) }
+        FullDescriptionMovieAdapter(
+            { movieId -> viewModel.loadSimilarMovieFullDescription(movieId) },
+            requireContext()
+        )
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initList()
         observeViewModelState()
-     TODO("добавить декораток к похожим фильмам. привестив  порядок адаптеры" +
-             "вынести в констануту нориер элемента для скролла, обработать ошибки, обработать состояние загрузки," +
-             "добавить анимацию при навигаци на данный фрагмент. добавить анимацию на добавление нового айтема в список" +
-             "во вью модели вынести муфи айди в костанту отрефакторить все что есть ")
     }
 
-
-
     private fun observeViewModelState() {
-        /* lifecycleScope.launchWhenStarted {
-             viewModel.isProgress.collect {
-                 binding.progressCircular.isVisible = it
-             }
+        lifecycleScope.launchWhenStarted {
+            viewModel.isProgress.collectLatest {
+                binding.progressBarHorizontal.isVisible = it
+            }
 
-         }*/
+        }
         lifecycleScope.launchWhenStarted {
             viewModel.movie.collectLatest { movie ->
                 scrollRwToTopAfterLoadData()
                 fullDescriptionMovieAdapter.setMovies(listOf(movie))
             }
         }
-        /*lifecycleScope.launchWhenStarted {
-            viewModel.similarMovie.collectLatest { listSimilarMovie ->
-                similarMovieAdapter.submitList(listSimilarMovie)
+        lifecycleScope.launchWhenStarted {
+            viewModel.error.collectLatest { errorMessage ->
+                makeSnack(errorMessage)
             }
-        }*/
+        }
+
+    }
+
+    private fun makeSnack(@StringRes message: Int) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
     }
 
     private fun scrollRwToTopAfterLoadData() {
         if (fullDescriptionMovieAdapter.itemCount >= 1) {
-            binding.fullDescriptionRecycler.smoothScrollToPosition(0)
+            binding.fullDescriptionRecycler.smoothScrollToPosition(ADAPTER_ITEM_POSITION_0)
         }
     }
 
@@ -67,6 +71,10 @@ class FullDescriptionFragment : Fragment(R.layout.fragment_full_description) {
             adapter = fullDescriptionMovieAdapter
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         }
+    }
+
+    companion object {
+        const val ADAPTER_ITEM_POSITION_0 = 0
     }
 
 }
