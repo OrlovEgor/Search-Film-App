6c93103b05cff48ed54a5b63f18dd25b0dee924c
Orlov Egor <dbn.orlov@gmail.com>
2022-10-12 15:32:25 +0300

    transfer logic from useCase

--- app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepositoryImpl.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepositoryImpl.kt
@@ -1,26 +1,37 @@
 package ru.orlovegor.search_film_app.data.repositories
 
+import android.content.Context
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.mapLatest
 import ru.orlovegor.search_film_app.data.remote.MovieApi
 import ru.orlovegor.search_film_app.data.paging.MoviesPageSource
-import ru.orlovegor.search_film_app.data.remote.models.MovieDto
+import ru.orlovegor.search_film_app.presentation.models.Movie
+import ru.orlovegor.search_film_app.presentation.models.mapToMovie
 import javax.inject.Inject
 
 class SearchMovieRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
+    @ApplicationContext private val context: Context
 ) : SearchMovieRepository {
-    override fun getMovieByTittlePaging(tittle: String): Pager<Int, MovieDto> {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override suspend fun getMovieByTittlePagingData(tittle: String): PagingData<Movie> {
         return Pager(
-            config = PagingConfig(
-                pageSize = 5
-            ),
+            config = PagingConfig(pageSize = 5),
             pagingSourceFactory = { MoviesPageSource(movieApi, tittle) }
-        )
+        ).flow
+            .mapLatest {pagingData -> pagingData.map { movieDto -> movieDto.mapToMovie(context) }}
+            .flowOn(Dispatchers.IO)
+            .first()
     }
 
-   suspend fun getFullDescriptionMovieByID(id: Long){
-        movieApi.getFullDescriptionMovie(id)
-    }
 
 }
