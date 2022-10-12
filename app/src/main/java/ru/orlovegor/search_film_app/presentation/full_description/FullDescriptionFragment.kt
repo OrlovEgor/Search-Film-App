adaab2db5eaf85ec9ad2d985c1fb26c133e85b9a
Orlov Egor <dbn.orlov@gmail.com>
2022-10-12 15:36:25 +0300

    Add ui logic model and layout

--- app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionFragment.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionFragment.kt
@@ -1,10 +1,15 @@
 package ru.orlovegor.search_film_app.presentation.full_description
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import ru.orlovegor.search_film_app.R
 import ru.orlovegor.search_film_app.databinding.FragmentFullDescriptionBinding
 
@@ -12,8 +17,56 @@ import ru.orlovegor.search_film_app.databinding.FragmentFullDescriptionBinding
 class FullDescriptionFragment : Fragment(R.layout.fragment_full_description) {
 
     private val binding: FragmentFullDescriptionBinding by viewBinding()
+    private val viewModel: FullDescriptionViewModel by viewModels()
+    private val fullDescriptionMovieAdapter by lazy(LazyThreadSafetyMode.NONE) {
+        FullDescriptionMovieAdapter { movieId -> viewModel.loadSimilarMovieFullDescription(movieId) }
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initList()
+        observeViewModelState()
+     TODO("добавить декораток к похожим фильмам. привестив  порядок адаптеры" +
+             "вынести в констануту нориер элемента для скролла, обработать ошибки, обработать состояние загрузки," +
+             "добавить анимацию при навигаци на данный фрагмент. добавить анимацию на добавление нового айтема в список" +
+             "во вью модели вынести муфи айди в костанту отрефакторить все что есть ")
     }
+
+
+
+    private fun observeViewModelState() {
+        /* lifecycleScope.launchWhenStarted {
+             viewModel.isProgress.collect {
+                 binding.progressCircular.isVisible = it
+             }
+
+         }*/
+        lifecycleScope.launchWhenStarted {
+            viewModel.movie.collectLatest { movie ->
+                scrollRwToTopAfterLoadData()
+                fullDescriptionMovieAdapter.setMovies(listOf(movie))
+            }
+        }
+        /*lifecycleScope.launchWhenStarted {
+            viewModel.similarMovie.collectLatest { listSimilarMovie ->
+                similarMovieAdapter.submitList(listSimilarMovie)
+            }
+        }*/
+    }
+
+    private fun scrollRwToTopAfterLoadData() {
+        if (fullDescriptionMovieAdapter.itemCount >= 1) {
+            binding.fullDescriptionRecycler.smoothScrollToPosition(0)
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun initList() {
+        with(binding.fullDescriptionRecycler) {
+            adapter = fullDescriptionMovieAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
 }
--- app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionViewModel.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/full_description/FullDescriptionViewModel.kt
@@ -1,12 +1,70 @@
 package ru.orlovegor.search_film_app.presentation.full_description
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import ru.orlovegor.search_film_app.data.repositories.FullDescriptionRepository
+import ru.orlovegor.search_film_app.presentation.models.Movie
+import ru.orlovegor.search_film_app.presentation.models.SimilarMovie
+import ru.orlovegor.search_film_app.utils.ResultWrapper
 import javax.inject.Inject
 
 @HiltViewModel
-class FullDescriptionViewModel @Inject constructor (
+class FullDescriptionViewModel @Inject constructor(
+    private val stateNavArgs: SavedStateHandle,
+    private val fullDescriptionRepository: FullDescriptionRepository
 
-): ViewModel() {
+) : ViewModel() {
+
+    private val _movie = MutableSharedFlow<Movie>()
+    private val _movieId = MutableStateFlow(stateNavArgs.get<Long>("movieId"))
+    private val _similarMovies = MutableSharedFlow<List<SimilarMovie>>()
+    private val _isProgress = MutableStateFlow(false)
+
+    val movie = _movie.asSharedFlow()
+    private val movieId = _movieId.asStateFlow()
+    val similarMovie = _similarMovies.asSharedFlow()
+    val isProgress = _isProgress.asStateFlow()
+
+    init {
+        viewModelScope.launch { load() }
+    }
+
+    private suspend fun load() {
+        if (movieId.value == null) {
+            return
+        } else {
+            movieId
+                .onEach {
+                    _isProgress.value = true
+                    when (val data = fullDescriptionRepository.getFullDescriptionMovieById(it!!)) {
+                        is ResultWrapper.Success -> {
+                            _movie.emit(data.value)
+                            _similarMovies.emit(data.value.similarMovie)
+                        }
+                        is ResultWrapper.Error -> {
+                            Log.d("VM", "Error")
+                        }
+                        is ResultWrapper.NetworkError -> {
+                            Log.d("VM", "NetworkError")
+                        }
+                    }
+                }
+                .onEach { _isProgress.value = false }
+                .flowOn(Dispatchers.IO)
+                .launchIn(viewModelScope)
+        }
+    }
+
+    fun loadSimilarMovieFullDescription(movieId: Long) {
+        viewModelScope.launch {
+            _movieId.emit(movieId)
+        }
+    }
 
 }
