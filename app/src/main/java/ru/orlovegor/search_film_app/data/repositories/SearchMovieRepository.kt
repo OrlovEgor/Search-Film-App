24fb1807de2af9c977940d90042ecd1235f615fb
Orlov Egor <dbn.orlov@gmail.com>
2022-10-17 01:15:24 +0300

    add insert delete and fetch movie

--- app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepository.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepository.kt
@@ -1,11 +1,19 @@
 package ru.orlovegor.search_film_app.data.repositories
 
 import androidx.paging.PagingData
+import ru.orlovegor.search_film_app.data.local.models.MovieLocal
 import ru.orlovegor.search_film_app.presentation.models.Movie
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities
 
 
 interface SearchMovieRepository {
 
   suspend fun getMovieByTittlePagingData(tittle: String): PagingData<Movie>
+
+  suspend fun insertMovie(movie: Movie): Boolean
+
+  suspend fun removeMovie(movie: Movie): Boolean
+
+  suspend fun getLocalMovies(): List<Movie>
 
 }
