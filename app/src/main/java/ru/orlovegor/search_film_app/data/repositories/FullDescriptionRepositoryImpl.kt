82e459e930cf2b4cc390f06f951aafe1adff4091
Orlov Egor <dbn.orlov@gmail.com>
2022-10-16 22:23:07 +0300

    delete try catch

--- app/src/main/java/ru/orlovegor/search_film_app/data/repositories/FullDescriptionRepositoryImpl.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/repositories/FullDescriptionRepositoryImpl.kt
@@ -2,27 +2,25 @@ package ru.orlovegor.search_film_app.data.repositories
 
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
-import retrofit2.HttpException
-import ru.orlovegor.search_film_app.utils.ResultWrapper
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import ru.orlovegor.search_film_app.data.remote.MovieApi
+import ru.orlovegor.search_film_app.di.IoDispatcher
 import ru.orlovegor.search_film_app.presentation.models.Movie
 import ru.orlovegor.search_film_app.presentation.models.mapToMovie
+import ru.orlovegor.search_film_app.utils.ResultWrapper
 import ru.orlovegor.search_film_app.utils.safeApiCall
-import java.io.IOException
 import javax.inject.Inject
 
 class FullDescriptionRepositoryImpl@Inject constructor(
     private val movieApi: MovieApi,
-    @ApplicationContext private val context: Context
+    @ApplicationContext private val context: Context,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : FullDescriptionRepository {
     override suspend fun getFullDescriptionMovieById(movieId: Long): ResultWrapper<Movie> =
-        try {  safeApiCall {  movieApi.getFullDescriptionMovie(movieId).mapToMovie(context)}
-        } catch (i: HttpException) {
-            ResultWrapper.Error(i)
-        } catch (i: IOException) {
-            ResultWrapper.NetworkError
+        withContext(dispatcher) {
+            safeApiCall { movieApi.getFullDescriptionMovie(movieId).mapToMovie(context) }
         }
-
 }
 
 
