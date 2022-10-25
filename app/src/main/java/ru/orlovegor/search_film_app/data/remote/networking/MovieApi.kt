344889a10c6f047208cc7c112501c3b3aeea2c99
Orlov Egor <dbn.orlov@gmail.com>
2022-10-25 21:51:02 +0300

    moved

--- app/src/main/java/ru/orlovegor/search_film_app/data/remote/networking/MovieApi.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/remote/networking/MovieApi.kt
@@ -1,9 +1,8 @@
-package ru.orlovegor.search_film_app.data.remote
+package ru.orlovegor.search_film_app.data.remote.networking
 
 import androidx.annotation.IntRange
 import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 import ru.orlovegor.search_film_app.data.remote.models.MovieDto
 import ru.orlovegor.search_film_app.data.remote.models.RemoteSearchResult
 
@@ -21,6 +20,19 @@ interface MovieApi {
     suspend fun getFullDescriptionMovie(
        @Query("search") id:Long
     ): MovieDto
+
+    @GET("/movie")
+    suspend fun getMovies(
+        @QueryMap tittle: Map<String, String>,
+        @QueryMap year: Map<String, String>,
+        @Query("page") @IntRange(from = 1) page: Int = 1,
+        @Query("limit") @IntRange(from = 1, to = MAX_PAGE_SIZE.toLong()) pageSize: Int = DEFAULT_PAGE_SIZE,
+        @Query("sortField[]") sortFieldKp: String = "votes.kp",
+        @Query("sortField[]") sortFieldPremiere: String = "premiere.world",
+        @Query("sortType[]") sortType:String = "-1",
+        @Query("sortType[]") sortTypeR: String = "-1",
+        @Query("isStrict") isStrict: Boolean = false
+    ):Response<RemoteSearchResult>
 
     companion object {
         const val DEFAULT_PAGE_SIZE = 10
