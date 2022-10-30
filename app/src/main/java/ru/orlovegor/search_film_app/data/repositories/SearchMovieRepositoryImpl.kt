2d3bb85f1f48c368ac2b270de564399e8ad2d66e
Orlov Egor <dbn.orlov@gmail.com>
2022-10-30 22:58:07 +0300

    Change logic

--- app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepositoryImpl.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/repositories/SearchMovieRepositoryImpl.kt
@@ -1,10 +1,7 @@
 package ru.orlovegor.search_film_app.data.repositories
 
 import android.content.Context
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.map
+import androidx.paging.*
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -12,9 +9,10 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.mapLatest
 import ru.orlovegor.search_film_app.data.paging.MoviesPageSource
-import ru.orlovegor.search_film_app.data.remote.MovieApi
+import ru.orlovegor.search_film_app.data.remote.networking.MovieApi
 import ru.orlovegor.search_film_app.presentation.models.Movie
 import ru.orlovegor.search_film_app.presentation.models.mapToMovie
+import ru.orlovegor.search_film_app.utils.RequestQueryConstructor
 import javax.inject.Inject
 
 class SearchMovieRepositoryImpl @Inject constructor(
@@ -23,10 +21,23 @@ class SearchMovieRepositoryImpl @Inject constructor(
 ) : SearchMovieRepository {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    override suspend fun getMovieByTittlePagingData(tittle: String): PagingData<Movie> {
+    override suspend fun getMoviesPagingData(
+        tittle: RequestQueryConstructor,
+        year: RequestQueryConstructor,
+        genre: RequestQueryConstructor,
+        rating: RequestQueryConstructor
+    ): PagingData<Movie> {
         return Pager(
             config = PagingConfig(pageSize = 5),
-            pagingSourceFactory = { MoviesPageSource(movieApi, tittle) }
+            pagingSourceFactory = {
+                MoviesPageSource(
+                    movieApi,
+                    tittle.makeQuery(),
+                    year.makeQuery(),
+                    genre.makeQuery(),
+                    rating.makeQuery()
+                )
+            }
         ).flow
             .mapLatest { pagingData -> pagingData.map { movieDto -> movieDto.mapToMovie(context) } }
             .flowOn(Dispatchers.IO)
