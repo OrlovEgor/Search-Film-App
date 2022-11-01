0afc612745ca78cd38027d55991a470d3133b8d1
Orlov Egor <dbn.orlov@gmail.com>
2022-11-02 00:28:48 +0300

    Working version of the screen with sorting. view listeners are based on Flow

--- app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MovieSearchViewModel.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MovieSearchViewModel.kt
@@ -5,11 +5,12 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import ru.orlovegor.search_film_app.data.repositories.SearchMovieRepository
 import ru.orlovegor.search_film_app.presentation.models.Movie
+import ru.orlovegor.search_film_app.utils.QueryType
+import ru.orlovegor.search_film_app.utils.RequestQueryConstructor
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,18 +18,58 @@ class MovieSearchViewModel @Inject constructor(
     private val searchMovieRepository: SearchMovieRepository
 ) : ViewModel() {
 
-    private val _query = MutableSharedFlow<String>()
-    private val query = _query.asSharedFlow()
+    private val _name = MutableStateFlow("")
+    private val _year = MutableStateFlow("")
+    private val _genre = MutableStateFlow("")
+    private val _rating = MutableStateFlow("")
+
+    private val _movies = MutableStateFlow(PagingData.empty<Movie>())
+    val movies = _movies.asStateFlow()
+
+    init {
+        searchMovies()
+    }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    val movies: StateFlow<PagingData<Movie>> = query
-        .mapLatest { searchMovieRepository.getMovieByTittlePagingData(it) }
-        .cachedIn(viewModelScope)
-        .stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
+    private fun searchMovies() {
+        combine(_name, _year, _genre, _rating) { flowArray ->
+            arrayOf(
+                RequestQueryConstructor(flowArray[0], QueryType.NAME),
+                RequestQueryConstructor(flowArray[1], QueryType.YEAR),
+                RequestQueryConstructor(flowArray[2], QueryType.Genre),
+                RequestQueryConstructor(flowArray[3], QueryType.Rating)
+            )
+        }.mapLatest {
+            searchMovieRepository.getMoviesPagingData(
+                it[0], it[1], it[2], it[3]
+            )
+        }
+            .cachedIn(viewModelScope)
+            .onEach { _movies.emit(it) }
+            .flowOn(Dispatchers.IO)
+            .launchIn(viewModelScope)
+    }
 
-    fun setQuery(query: String) {
+    fun getQueryData(
+        name: Flow<String>,
+        year: Flow<String>,
+        genre: Flow<String>,
+        rating: Flow<String>
+    ) {
         viewModelScope.launch {
-            _query.emit(query)
+            launch {
+                name.collectLatest { _name.value = it }
+            }
+            launch {
+                year.collectLatest { _year.value = it }
+            }
+            launch {
+                genre.collectLatest { _genre.value = it }
+            }
+            launch {
+                rating.collectLatest { _rating.value = it }
+            }
         }
     }
+
 }
--- app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MoviesSearchFragment.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/presentation/movie_search/MoviesSearchFragment.kt
@@ -2,18 +2,12 @@ package ru.orlovegor.search_film_app.presentation.movie_search
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
-import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
@@ -22,7 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.collectLatest
 import ru.orlovegor.search_film_app.R
 import ru.orlovegor.search_film_app.databinding.FragmentSearchMoviesBinding
 import ru.orlovegor.search_film_app.presentation.adapters.MovieAdapter
@@ -31,16 +25,6 @@ import java.util.*
 
 @AndroidEntryPoint
 class MoviesSearchFragment : Fragment(R.layout.fragment_search_movies) {
-
-    private val textWatcher = object : TextWatcher{
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        }
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        }
-        override fun afterTextChanged(p0: Editable?) {
-        }
-
-    }
 
     private val binding: FragmentSearchMoviesBinding by viewBinding()
     private val viewModel: MovieSearchViewModel by viewModels()
@@ -72,10 +56,16 @@ class MoviesSearchFragment : Fragment(R.layout.fragment_search_movies) {
                 movieAdapter.submitData(it)
             }
         }
-       // binding.yearTextInput.addTextChangedListener(textWatcher)
-       // binding.genreInput.addTextChangedListener(textWatcher)
-       /// initSearchView()
-        getQuery()
+        sendDataToViewModel()
+    }
+
+    private fun sendDataToViewModel() {
+        viewModel.getQueryData(
+            binding.movieSearchView.queryTextListenerFlow(),
+            binding.yearTextInput.valueChangedFlow(),
+            binding.genreInput.valueGenreChangedFlow(),
+            binding.ratingSlider.onTouchListenerFlow()
+        )
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
@@ -93,43 +83,6 @@ class MoviesSearchFragment : Fragment(R.layout.fragment_search_movies) {
             layoutManager = LinearLayoutManager(requireContext())
             addItemDecoration(dividerDecorator)
         }
-    }
-
-    private fun sendData(){
-        val sliderValue = binding.ratingSlider.values
-        viewModel.sendData(
-            binding.movieSearchView.query.toString(),
-            binding.yearTextInput.text.toString(),
-            binding.genreInput.text.toString(),
-            "${sliderValue[0]}-${sliderValue[1]}"
-        )
-    }
-
-    private fun getQuery() {
-        val sliderValue = binding.ratingSlider.values
-            viewModel.setQuery(
-                binding.movieSearchView.queryTextListenerFlow().onStart {if (binding.movieSearchView.query.toString().isBlank())
-                    emit( "") },
-                binding.yearTextInput.valueChangedFlow().onStart { if (binding.yearTextInput.text.toString().isBlank())
-                    emit( "") },
-                binding.genreInput.valueGenreChangedFlow().onStart { if (binding.genreInput.text.toString().isBlank())
-                    emit( "" )},
-                binding.ratingSlider.onTouchListenerFlow().onStart { emit( "${sliderValue[0]}-${sliderValue[1]}") }
-            )
-    }
-
-
-   private fun initSearchView(){
-        binding.movieSearchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                sendData()
-                return true
-            }
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        }
-        )
     }
 
     private fun initErrorViewGroup() {
