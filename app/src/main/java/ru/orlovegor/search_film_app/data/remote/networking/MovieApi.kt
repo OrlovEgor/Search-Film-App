494cf95e2ff7da7248d448de71ddc712ae3126c4
Orlov Egor <dbn.orlov@gmail.com>
2022-11-02 00:31:46 +0300

    Refactoring, deleted unused fun

--- app/src/main/java/ru/orlovegor/search_film_app/data/remote/networking/MovieApi.kt
+++ app/src/main/java/ru/orlovegor/search_film_app/data/remote/networking/MovieApi.kt
@@ -8,35 +8,30 @@ import ru.orlovegor.search_film_app.data.remote.models.RemoteSearchResult
 
 interface MovieApi {
 
-    @GET("/movie?field=name&limit=20&sortField[]=votes.kp&sortField[]=premiere.world&sortType[]=-1&sortType[]=-1")
-    suspend fun getMovieByTittle(
-        @Query("search") tittle: String,
-        @Query("page") @IntRange(from = 1) page: Int = 1,
-        @Query("limit") @IntRange(from = 1, to = MAX_PAGE_SIZE.toLong()) pageSize: Int = DEFAULT_PAGE_SIZE,
-        @Query("isStrict") isStrict: Boolean = false
-    ): Response<RemoteSearchResult>
-
     @GET("/movie?field=id")
     suspend fun getFullDescriptionMovie(
        @Query("search") id:Long
     ): MovieDto
 
-    @GET("/movie")
+    @GET("/movie?")
     suspend fun getMovies(
         @QueryMap tittle: Map<String, String>,
         @QueryMap year: Map<String, String>,
+        @QueryMap genre: Map<String, String>,
+        @QueryMap rating: Map<String, String>,
         @Query("page") @IntRange(from = 1) page: Int = 1,
-        @Query("limit") @IntRange(from = 1, to = MAX_PAGE_SIZE.toLong()) pageSize: Int = DEFAULT_PAGE_SIZE,
+        @Query("limit") @IntRange(from = 10, to = MAX_PAGE_SIZE.toLong()) pageSize: Int = DEFAULT_PAGE_SIZE,
+        @Query("isStrict") isStrict: Boolean = false,
         @Query("sortField[]") sortFieldKp: String = "votes.kp",
         @Query("sortField[]") sortFieldPremiere: String = "premiere.world",
         @Query("sortType[]") sortType:String = "-1",
-        @Query("sortType[]") sortTypeR: String = "-1",
-        @Query("isStrict") isStrict: Boolean = false
+        @Query("sortType[]") sortTypeR: String = "-1"
     ):Response<RemoteSearchResult>
 
     companion object {
-        const val DEFAULT_PAGE_SIZE = 10
-        const val MAX_PAGE_SIZE = 15
+        const val DEFAULT_PAGE_SIZE = 20
+        const val MAX_PAGE_SIZE = 30
     }
+
 }
 
